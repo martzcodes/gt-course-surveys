37b1ee4295da175c13edf26a74381586cae87027
Mehmet Bajin <bajin.mehmet@gmail.com>
2015-11-06 13:00:13 -0600

    unknown workload support

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 var app = require('./server/routes');
 
-var server = app.listen(5000, function() {
+var server = app.listen(process.env.PORT || 5000, function() {
  console.log('Listening on port %d', server.address().port);
 });
--- app/assets/js/controllers/homeController.js
+++ app/assets/js/controllers/homeController.js
@@ -7,34 +7,22 @@ angular.module('surveyor').controller('HomeController',
           course.reviews = ReviewList(course.$id);
           course.reviews.$loaded()
             .then(function (reviewList) {
-              var totals = {
-                difficulty: 0,
-                workload: 0
-              };
+              var totals = { difficulty: 0, workload: 0 };
+              var counts = { difficulty: 0, workload: 0, total: 0 };
 
-              var counts = {
-                total: 0,
-                difficulty: 0,
-                workload: 0
-              };
-              
               angular.forEach(reviewList, function (review) {
                 counts.total++;
-
                 if (review.difficulty > 0) {
                   totals.difficulty += review.difficulty;
                   counts.difficulty++;
                 }
-
                 if (review.workload > 0) {
                   totals.workload += review.workload;
                   counts.workload++;
                 }
               });
 
-              if (counts.total > 0) {
-                course.reviewCount = counts.total;
-              }
+              course.reviewCount = counts.total;
               if (counts.difficulty > 0) {
                 course.averageDifficulty = totals.difficulty / counts.difficulty;
               }
