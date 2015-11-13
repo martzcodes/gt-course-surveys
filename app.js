b24ab190bda99648f979175d6e92560b5fab5b82
Mehmet Bajin <bajin.mehmet@gmail.com>
2015-11-13 09:53:24 -0600

    allow password change, remove voting

--- app.js
+++ app.js
@@ -2,5 +2,5 @@
 var app = require('./server/routes');
 
 var server = app.listen(process.env.PORT || 5000, function() {
- console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
