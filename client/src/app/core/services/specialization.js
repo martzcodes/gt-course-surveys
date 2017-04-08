c90a7fc5ed9988375faf5fe2af95d3aad703bcf7
Mehmet Bajin <mehmet.bajin@yahoo.com>
2017-04-07 20:02:20 -0700

    feat: specialization service

---
+++ client/src/app/core/services/specialization.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app.core')
+    .factory('Specialization', Specialization);
+
+  /** @ngInject */
+  function Specialization(Util) {
+    const ini = 'SPC';
+
+    const service = {
+      all,
+      get
+    };
+
+    return service;
+
+    //////////
+
+    async function all() {
+      const snapshot = await firebase.database().ref(ini).once('value');
+
+      return Util.many(snapshot);
+    }
+
+    async function get(id) {
+      const snapshot = await firebase.database().ref(ini)
+        .child(id)
+        .once('value');
+
+      return Util.one(snapshot);
+    }
+  }
+})();
